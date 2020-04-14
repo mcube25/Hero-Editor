fcc86ddd5209af4ffcf75a9acc98fff3610fa8b8
Muhammad Muhktar Musa <mcube3567@gmail.com>
2020-04-14 15:02:30 -0700

    display list

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
-hero: Hero = {
+  heroes = HEROES;
+  hero: Hero = {
   id: 1,
   name:'Windstorm'
+}
+selectedHero: Hero;
+onSelect(hero:Hero): void {
+  this.selectedHero = hero;
 }
   constructor() { }
 
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+
+export const HEROES: Hero[] = [
+{id: 11, name: 'dr nice'},
+{id: 12, name: 'Narco'},
+{id: 13, name: 'Bombasto'},
+{id: 14, name: 'Celeritas'},
+{id: 15, name: 'magneta'},
+{id: 16, name: 'rubberman'},
+{id: 17, name: 'dynama'},
+{id: 18, name: 'dr iq'},
+{id: 19, name: 'magma'},
+{id: 20, name: 'tornado'},
+];
